bca5097507ee0d9900c073d8855149037308dc94
Your Name <you@example.com>
2026-03-26 09:58:43 +0000

    add_0.py

---
+++ python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+guillaume@ubuntu:~/$ cat add_0.py
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
