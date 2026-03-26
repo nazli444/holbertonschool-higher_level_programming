2dd85c56e0a256289f89c969806f18308a36c5ae
Your Name <you@example.com>
2026-03-26 18:40:22 +0000

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(count))
     for n in range(1, len(argv)):
-        print("{}: {}".format(n, len(argv)))
+        print("{}: {}".format(n, argv[n]))
