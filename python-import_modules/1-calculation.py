acbd67e34cadc7d2d9edff1e72ea6eab7c130b90
Your Name <you@example.com>
2026-03-26 14:36:20 +0000

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from calculator_1 import add,sub,mul,div
-if __name__ = "__main__":
+if __name__ == "__main__":
     """a:first integer"""
     a = 10
     """b:second integer"""
