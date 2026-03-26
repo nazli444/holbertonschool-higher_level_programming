7fe6154ef16e7ff9351a18211f9eb8918974a245
Your Name <you@example.com>
2026-03-26 08:02:34 +0000

    8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    n=""
+    for k in str:
+        if "a" <= c <= "z":
+            n = n + chr(ord(c) - 32)
+        else:
+            n = n + c
+    print(n)
