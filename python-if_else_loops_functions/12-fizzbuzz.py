fa306021401e2fe882b7dd27da79543c346d4347
Your Name <you@example.com>
2026-03-26 08:53:52 +0000

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 def fizzbuzz():
     for n in range(1,100):
         if n % 3 == 0 and n % 5 == 0:
-            print("{}".format(FizzBuzz))
+            print("{}".format("FizzBuzz"))
         elif n % 3 == 0:
-            print("{}".format(Fizz))
+            print("{}".format("Fizz"))
         elif n % 5 == 0:
-            print("{}".format(Buzz))
+            print("{}".format("Buzz"))
         else:
             print(n)
     return 
