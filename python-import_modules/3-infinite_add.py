8df679669e10e8ea0708fe9a486bb72462674b9d
Your Name <you@example.com>
2026-03-26 19:01:08 +0000

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 import sys
 if __name__ == "__main__":
     k = 0
-    for n in range(0, len(sys.argv) + 1):
+    for n in range(0, len(sys.argv)):
         k = k + int(sys.argv[1])
     print("{}".format(k))
