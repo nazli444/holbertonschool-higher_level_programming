880e41e510be6b95141997cbce71dd90b2272f02
Your Name <you@example.com>
2026-03-26 18:38:22 +0000

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argv = sys.argv
+    count = len(argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for n in range(1, len(argv)):
+        print("{}: {}".format(n, len(argv)))
