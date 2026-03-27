1df30930de7c04e27a04373d9ad3c18213523143
Your Name <you@example.com>
2026-03-27 09:46:47 +0000

    6-print_matrix_integer.py

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for k in matrix:
+        for n in range(0, len(k)):
+            if n == len(k) - 1:
+                print("{:d}".format(k[n]), end="")
+            else:
+                print("{:d}".format(k[n]), end=" ")
