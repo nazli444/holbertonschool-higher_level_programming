38de294e2ae7699d24e577050de8a016dffac4d3
Your Name <you@example.com>
2026-03-27 13:56:14 +0000

    0-square_matrix_simple.py

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyton3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for n in matrix:
+        h = []
+        for k in n:
+            h.append(k ** 2)
+        new_matrix.append(h)
+    return new_matrix
