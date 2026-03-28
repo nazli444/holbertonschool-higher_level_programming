9023a4aa66eff19faa0c8568fe941ad825cc12d4
Your Name <you@example.com>
2026-03-28 16:58:21 +0000

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     best_key = None
     max_value = None
     for key in a_dictionary:
-        if max_value = None or a_dictionary[key] > max_value:
+        if max_value is None or a_dictionary[key] > max_value:
             max_value = a_dictionary[key]
             best_key = key
     return best_key
