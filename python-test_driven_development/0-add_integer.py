1a404f2a103b9c0bd2e4a468ff95c55e18b8e6a3
Your Name <you@example.com>
2026-03-28 22:21:52 +0000

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    """Module for adding two integers.
-        add a and b two integers
-        a and b must be integer and float
-        we should cast b integer
-        Raises TypeError if a or b are not integers or floats."""
+    """
+    Adds two integers a and b.
+    Casts floats to integers.
+    Raises TypeError if a or b are not integers or floats.
+    Returns the integer sum of a and b.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
