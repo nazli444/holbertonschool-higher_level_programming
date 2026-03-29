83bccf608fc485043b14e4fcd45386f5415f72f0
Your Name <you@example.com>
2026-03-29 07:13:15 +0000

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,8 +10,8 @@ def add_integer(a, b=98):
     Raises TypeError if a or b are not integers or floats.
     Returns the integer sum of a and b.
     """
-    if type(a) not in [int, float]:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
