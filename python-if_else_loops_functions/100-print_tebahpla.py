1ba31f7d802c8f2c192c09c59b709317888931d0
Your Name <you@example.com>
2026-03-29 08:02:31 +0000

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    print("{}".format(chr(122 - i) if i % 2 == 0 else chr(65 + i)), end="")
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2 == 0:
+        char = i
+    else:
+        char = i - 32
+    print("{:c}".format(char), end="")
