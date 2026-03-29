782fab0a58f174fc081d379a2e0018040335d87e
Your Name <you@example.com>
2026-03-29 07:09:03 +0000

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -14,4 +14,8 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+    if a == float('inf') or a == float('-inf') or a != a:
+        raise OverflowError("cannot convert float infinity to integer")
+    if b == float('inf') or b == float('-inf') or b != b:
+        raise OverflowError("cannot convert float infinity to integer")
     return int(a) + int(b)
