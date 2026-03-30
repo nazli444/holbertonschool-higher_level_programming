b11c3e61972159b7b1320a863051292fff4b7329
Your Name <you@example.com>
2026-03-30 21:53:52 +0000

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Module that defines an empty Square class.
+This module contains a single class: Square.
+"""
+
+
 class Square:
     """An empty class that defines a square"""
     pass
