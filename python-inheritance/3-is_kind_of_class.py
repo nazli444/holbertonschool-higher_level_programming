f793f4f8103ddedef49ae03b1953587d51f2c34b
Your Name <you@example.com>
2026-04-11 21:45:48 +0000

    3-is_kind_of_class.py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Module that checks if an object is exactly an instance of a class"""
+"""Module that checks if an object is an instance of a class or inherited"""
 
 
-def is_same_class(obj, a_class):
-    """Returns True if obj is exactly an instance of a_class"""
-    return type(obj) is a_class
+def is_kind_of_class(obj, a_class):
+    """Returns True if obj is instance of a_class or inherited from it"""
+    return isinstance(obj, a_class)
