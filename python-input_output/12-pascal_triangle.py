92f403efeb7f200464a2b26398348db2db152c62
Your Name <you@example.com>
2026-04-18 13:24:09 +0000

    12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that generates Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """Return Pascal's triangle of size n"""
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1]
+
+        if triangle:
+            last_row = triangle[-1]
+            for j in range(len(last_row) - 1):
+                row.append(last_row[j] + last_row[j + 1])
+            row.append(1)
+
+        triangle.append(row)
+
+    return triangle
