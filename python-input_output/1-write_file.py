e892169330195cde7738fc8aeb1002879e394526
Your Name <you@example.com>
2026-04-18 11:33:56 +0000

    1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that writes a string to a UTF-8 text file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a file and returns the number of characters written"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
