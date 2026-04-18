9dcb9b5073dcb4953e7a9bb56c0f620af9e51d96
Your Name <you@example.com>
2026-04-18 12:44:41 +0000

    6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Module that loads an object from a JSON file"""
-
+"""Module that loads a JSON object from a file"""
 
 import json
+
+
 def load_from_json_file(filename):
     """Create a Python object from a JSON file"""
     with open(filename, "r", encoding="utf-8") as f:
-    return jso.load(f)
+        return json.load(f)
