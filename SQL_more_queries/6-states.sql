798251e5ef90ef71372399f90dd3368867738487
Your Name <you@example.com>
2026-05-03 13:01:17 +0000

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,4 @@
 -- Create database hbtn_0d_usa if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- Use the database
 -- Create table states if it does not exist
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
