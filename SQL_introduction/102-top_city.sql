6a7c3bc84ff8f16e7738caaae7e1395b0bc91f35
Your Name <you@example.com>
2026-05-03 17:16:08 +0000

    102-top_city.sql

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,10 +1,10 @@
--- 19. Top 3 cities temperatures in July and August
+-- 19. Top 3 cities temperatures during July and August
 
 SELECT
     city,
     AVG(value) AS avg_temp
 FROM temperatures
-WHERE MONTH(date) IN (7, 8)
+WHERE month IN (7, 8)
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
