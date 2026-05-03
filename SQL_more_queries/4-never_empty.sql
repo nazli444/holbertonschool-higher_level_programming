57217f63081592daf3056348d7e089072a4ba2eb
Your Name <you@example.com>
2026-05-03 15:16:12 +0000

    4-never_empty.sql

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- Create table id_not_null if it does not exist
-CREATE TABLE IF NOT EXIST id_not_null(id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
