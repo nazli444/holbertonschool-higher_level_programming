6e8e99425fd2352d609f0a67749117247d3f8961
Your Name <you@example.com>
2026-05-03 17:11:18 +0000

    100-move_to_utf8.sql

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,17 +1,11 @@
--- 17. Convert database, table and field to UTF8 (utf8mb4)
+-- 17. Convert database and table to UTF8 (utf8mb4)
 
--- Convert database
+USE hbtn_0c_0;
+
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
--- Convert table
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
--- Convert specific column
-ALTER TABLE first_table
-MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
