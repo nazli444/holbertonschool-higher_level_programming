f4a63fc4694c6101c35ec9ac61923fb79c783769
Your Name <you@example.com>
2026-05-04 19:28:45 +0000

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This script connects to a MySQL database and prints all states ordered by id"""
+"""Script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 if __name__ == "__main__":
