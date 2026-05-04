1460a4c8d9ba3fc5dbf4515e2448e4543d9d3661
Your Name <you@example.com>
2026-05-04 19:38:49 +0000

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect("localhost", sys.argv[1],
                          sys.argv[2], sys.argv[3], 3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id")
 
     for row in cur.fetchall():
         print(row)
