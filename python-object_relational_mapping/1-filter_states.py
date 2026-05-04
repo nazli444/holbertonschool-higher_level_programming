7c271ec9237ddb61edc4da9d2601b3d077a9ca21
Your Name <you@example.com>
2026-05-04 19:33:45 +0000

    1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Lists states with names starting with N"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3], 3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
