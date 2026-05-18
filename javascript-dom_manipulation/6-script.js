8eb3b08bdf95974d442d6fcd9c0242a0c411e7e3
Your Name <you@example.com>
2026-05-18 12:27:20 +0000

    6-script.js

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,13 @@
+// 6-script.js
+
+const url = "https://swapi-api.hbtn.io/api/people/5/?format=json";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const characterDiv = document.querySelector("#character");
+    characterDiv.textContent = data.name;
+  })
+  .catch((error) => {
+    console.error("Error fetching character:", error);
+  });
