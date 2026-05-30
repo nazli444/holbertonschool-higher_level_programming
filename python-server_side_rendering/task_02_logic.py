1304303a12345e5f297d12adfd52ead2795fb5f8
Your Name <you@example.com>
2026-05-30 09:18:02 +0200

    task_02_logic.py

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Home Page"
+
+
+@app.route('/items')
+def items():
+    with open('items.json') as f:
+        data = json.load(f)
+        items_list = data.get("items", [])
+
+    return render_template('items.html', items=items_list)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
