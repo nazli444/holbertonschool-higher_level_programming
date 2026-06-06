198718acadc40b4eb3085c7d8a8ae979d671ec63
Your Name <you@example.com>
2026-06-06 11:27:38 +0000

    11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2).map(Number);
+
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  let max = -Infinity;
+  let second = -Infinity;
+
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] > max) {
+      second = max;
+      max = args[i];
+    } else if (args[i] > second && args[i] < max) {
+      second = args[i];
+    }
+  }
+
+  console.log(second);
+}
